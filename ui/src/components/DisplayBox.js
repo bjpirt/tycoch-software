459d81c47a7af5a3e8b65ac3163ec53ab0cff9f1
Ben Pirt <ben@pirt.co.uk>
2019-02-16 11:17:22 +0000

    Re-scale the battery percentage

--- ui/src/components/DisplayBox.js
+++ ui/src/components/DisplayBox.js
@@ -5,12 +5,20 @@ class DisplayBox extends Component {
     let percent = 100 * this.props.value/this.props.max;
     return {backgroundImage: `linear-gradient(to top, ${this.props.graphColor} ${percent}%, rgba(0,0,0,0) ${percent}%)`}
   }
+
+  getValue(){
+    let val = this.props.value
+    if(this.props.rescale && typeof this.props.min !== 'undefined'){
+      val = ((val - this.props.min) * 100) / (this.props.max - this.props.min)
+    }
+    return val.toFixed(this.props.precision)
+  }
   
   render(){
     return (
       <div className="displayBox" style={this.graphStyle()}>
         <h3>{ this.props.name }</h3>
-        <span>{this.props.value.toFixed(this.props.precision)}{this.props.unit}</span>
+        <span>{this.getValue()}{this.props.unit}</span>
       </div>
     )
   }
