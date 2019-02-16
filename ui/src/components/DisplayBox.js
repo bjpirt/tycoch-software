5240ef562010b83f6c43b2f52eee174c1409baaa
Ben Pirt <ben@pirt.co.uk>
2019-02-16 22:37:20 +0000

    Correctly scale the displaybox bar

--- ui/src/components/DisplayBox.js
+++ ui/src/components/DisplayBox.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 class DisplayBox extends Component {
   graphStyle(){
-    let percent = 100 * this.props.value/this.props.max;
+    let percent = 100 * this.getValue()/this.props.max;
     return {backgroundImage: `linear-gradient(to top, ${this.props.graphColor} ${percent}%, rgba(0,0,0,0) ${percent}%)`}
   }
 
