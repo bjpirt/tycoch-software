2c91278f0dd957ba9e5a262bf11aae18e1d70c17
Ben Pirt <ben@pirt.co.uk>
2019-12-22 12:45:57 +0000

    Update the sync script and add a logs db to influx

--- bin/sync-data.py
+++ bin/sync-data.py
@@ -1,34 +1,124 @@
+#!/usr/bin/env python
+
+import sys
 import json
+import time
+from datetime import datetime
+from datetime import timedelta
+
 try:
   from urllib import urlencode
 except:
   from urllib.parse import urlencode
 
 try:
-  from urllib2 import urlopen
+  from urllib2 import Request, urlopen
 except:
-  from urllib.request import urlopen
+  from urllib.request import Request, urlopen
+
+if len(sys.argv) < 2:
+  sys.exit(1)
+else:
+  remote_url = sys.argv[1]
 
 influx_host = 'localhost'
 
 flatten = lambda l: [item for sublist in l for item in sublist]
 
-def query_influx(q):
-  query = urlencode({'q': q})
-  result = urlopen("http://" + influx_host + ":8086/query?" + query).read()
+def query_influx(q, epoch=False):
+  query = {'q': q}
+  if epoch:
+    query['epoch'] = 's'
+  result = urlopen("http://" + influx_host + ":8086/query?" + urlencode(query)).read()
   return json.loads(result)['results'][0]
 
-groups = flatten(query_influx("SHOW MEASUREMENTS ON tycoch")['series'][0]['values'])
-results = []
+def write_time_log(t):
+  data = ('uploads upload=1 %s' % (t)).encode('UTF-8')
+  req = Request("http://" + influx_host + ":8086/write?db=logs&precision=s", data=data)
+  request = urlopen(req)
 
-for measurement in groups:
-  if measurement != 'disk':
-    print(measurement)
-    print('SELECT * FROM "tycoch"."forever"."%s" WHERE time > now() - 1h' % measurement)
-    results.append(query_influx('SELECT * FROM "tycoch"."forever"."%s" WHERE time > now() - 1h' % measurement))
+def get_last_upload():
+  return query_influx('SELECT last("upload") FROM "logs"."ninetyday"."uploads"')
 
-results.append(query_influx('SELECT "mean_10m_used_percent" FROM "tycoch"."forever"."disk" WHERE time > now() - 1h AND "path"=\'/data\''))
-results.append(query_influx('SELECT "mean_10m_used_percent" FROM "tycoch"."forever"."disk" WHERE time > now() - 1h AND "path"=\'/data-ramdisk\''))
+def check_first_time():
+  last_upload = get_last_upload()
+  if 'series' not in last_upload.keys():
+    # Get first record and upload by days
+    first_record = query_influx('SELECT first("mean_1m_batt-voltage") FROM "tycoch"."ninetyday"."ac"', True)
+    t = first_record['series'][0]['values'][0][0]
+    write_time_log(t)
+    print(first_record['series'][0]['values'][0][0])
 
-print(results)
+def get_start_time():
+  check_first_time()
+  last_upload = query_influx('SELECT last("upload") FROM "logs"."ninetyday"."uploads"')
+  return datetime.strptime(last_upload['series'][0]['values'][0][0], "%Y-%m-%dT%H:%M:%SZ")
+
+def fetch_data(start_time):
+  results = []
+  groups = flatten(query_influx("SHOW MEASUREMENTS ON tycoch")['series'][0]['values'])
+  for measurement in groups:
+    if measurement != 'disk':
+      results.append(query_influx('SELECT * FROM "tycoch"."forever"."%s" WHERE time > \'%sZ\'' % (measurement, start_time.isoformat())))
+
+  results.append(query_influx('SELECT "mean_10m_used_percent" FROM "tycoch"."forever"."disk" WHERE time > \'%sZ\' AND "path"=\'/data\'' % (start_time.isoformat())))
+  results.append(query_influx('SELECT "mean_10m_used_percent" FROM "tycoch"."forever"."disk" WHERE time > time > \'%sZ\' AND "path"=\'/data-ramdisk\'' % (start_time.isoformat())))
+  return normalise_data(results)
+  
+def write_log(data):
+  sent_times = list(data['values'].keys())
+  sent_times.sort()
+
+  last_update = datetime.strptime(sent_times[-1], "%Y-%m-%dT%H:%M:%SZ")
+  write_time_log(int(time.mktime(last_update.timetuple())))
+  
+  print("%d values sent between %s and %s" % (len(data['values']), sent_times[0], sent_times[-1]))
+
+def reformat_columns(name, columns):
+  filtered_columns = filter(lambda col: col != 'time', columns)
+  return list(map(lambda col: f'{name}-{col.replace("mean_10m_", "")}', filtered_columns))
+
+def round_values(values):
+  return list(map(lambda val: round(val, 1) if val else None, values))
+
+def normalise_data(data):
+  output = {'values': {}, 'columns': []}
+  for group in data:
+    if 'series' in group:
+      for series in group['series']:
+        output['columns'].extend(reformat_columns(series['name'], series['columns']))
+        for value in series['values']:
+          if value[0] in output['values']:
+            output['values'][value[0]].extend(round_values(value[1:]))
+          else:
+            output['values'][value[0]] = round_values(value[1:])
+  return output
+  
+def limit_data_size(data, limit):
+  if len(data['values'].keys()) > limit:
+    filter_keys = list(data['values'].keys())
+    filter_keys.sort()
+    f = lambda i: i[0] in filter_keys[0:limit]
+    filtered = list(filter(f, data['values'].items()))
+    data['values'] = dict(filtered)
+  return data
+
+def send_data(data):
+  json_data = json.dumps(data).encode('UTF-8')
+  req = Request(remote_url, data=json_data)
+  req.add_header('Content-Type', 'application/json')
+  request = urlopen(req)
+
+def upload():
+  start_time = get_start_time()
+  data = fetch_data(start_time)
+  if len(data['values']) > 0:
+    data = limit_data_size(data, 150)
+    try:
+      send_data(data)
+      write_log(data)
+    except:
+      print("Error sending data")
+
+upload()
 
