5e614d7e854953812a3006624580857bcddfb388
Ben Pirt <ben@pirt.co.uk>
2019-12-30 12:14:03 +0000

    Don't re-scale the battery capacity

--- ui/src/components/ElectricityDisplay.jsx
+++ ui/src/components/ElectricityDisplay.jsx
@@ -9,7 +9,7 @@ class ElectricityDisplay extends Component {
       <div className="electricityDisplay">
         <DisplayBox name="Solar Generation" value={this.props.data.pv['power-generation']} unit="W" precision={0} max={2500} graphColor="#F2D599" />
         <Arrow />
-        <DisplayBox name="Battery Bank" value={this.props.data.battery['state-of-charge']} unit="%" precision={1} max={100} min={95} rescale={true} graphColor="#A8EFAA"  />
+        <DisplayBox name="Battery Bank" value={this.props.data.battery['state-of-charge']} unit="%" precision={1} max={100} min={95} rescale={false} graphColor="#A8EFAA"  />
         <Arrow />
         <DisplayBox name="AC Consumption" value={this.props.data.ac['power-usage']} unit="W" precision={0} max={4000} graphColor="#EFA8A8"  />
       </div>
